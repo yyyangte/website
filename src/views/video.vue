1b8aba205c0cb133013cbc0b5161f4a93f498f47
yte <yang13210999198@aliyun.com>
2020-11-04 16:57:24 +0800

    更改

--- src/views/video.vue
+++ src/views/video.vue
@@ -16,7 +16,7 @@
         layout="prev, pager, next"
         :current-page="page + 1"
         @current-change="changePage"
-        :page-count="10"
+        :page-count="4"
       >
       </el-pagination>
     </div>
@@ -27,7 +27,7 @@
       @close="close()"
     >
       <video
-      ref="video"
+        ref="video"
         :src="path"
         controls="controls"
         autoplay="autoplay"
@@ -53,16 +53,38 @@ export default {
           },
           {
             name: "中华人员共和国保守秘密法",
-            path: require("../assets/video/5分钟看懂保密法--宣传教育.mp4"),
+            path: require("../assets/video/中华人员共和国保守秘密法.mp4"),
           },
           {
-            name: "保密法规宣传系列之一：当心手机泄密",
-            path: require("../assets/video/5分钟看懂保密法--宣传教育.mp4"),
+            name: "互联网计算机安全保密须知",
+            path: require("../assets/video/互联网计算机安全保密须知.mp4"),
           },
           {
-            name: "保密法规宣传(二):谨防12种保密违法违规行为",
-            path: require("../assets/video/保密警示案例系列之三：兼职背后的陷阱.mp4"),
+            name: "保密法规宣传系列之一：当心手机泄密：当心手机泄密",
+            path: require("../assets/video/保密法规宣传系列之一：当心手机泄密.mp4"),
           },
+          {
+            name: "保密法规宣传系列之二：谨防12种保密违法违规行为",
+            path: require("../assets/video/保密法规宣传系列之二：谨防12种保密违法违规行为.mp4"),
+          },
+          {
+            name: "保密要害部门部位保密管理须知",
+            path: require("../assets/video/保密要害部门部位保密管理须知.mp4"),
+          },
+          {
+            name: "保密要害部门部位保密管理须知2",
+            path: require("../assets/video/保密要害部门部位保密管理须知2.mp4"),
+          },
+          {
+            name: "保密警示案例系列之一：机关门户网站的隐忧",
+            path: require("../assets/video/保密警示案例系列之一：机关门户网站的隐忧.mp4"),
+          },
+          {
+            name: "保密警示案例系列之二：网盘上的密件",
+            path: require("../assets/video/保密警示案例系列之二：网盘上的密件.mp4"),
+          },
+        ],
+        [
           {
             name: "保密警示案例系列之三：兼职背后的陷阱",
             path: require("../assets/video/保密警示案例系列之三：兼职背后的陷阱.mp4"),
@@ -72,30 +94,88 @@ export default {
             path: require("../assets/video/保密警示案例系列之四：变味的科研合作.mp4"),
           },
           {
-            name: "保保密警示案例系列之一：机关门户网站的隐忧",
-            path: require("../assets/video/5分钟看懂保密法--宣传教育.mp4"),
+            name: "国家秘密载体管理必知",
+            path: require("../assets/video/国家秘密载体管理必知.mp4"),
           },
           {
-            name: "保密法规宣传系列之二：谨防12种保密违法违规行为2",
-            path: require("../assets/video/5分钟看懂保密法--宣传教育.mp4"),
+            name: "图说保密法",
+            path: require("../assets/video/图说保密法.mp4"),
           },
           {
-            name: "保密法规宣传系列之二：谨防12种保密违法违规行为3",
-            path: require("../assets/video/5分钟看懂保密法--宣传教育.mp4"),
+            name: "手机使用安全保密须知",
+            path: require("../assets/video/手机使用安全保密须知.mp4"),
+          },
+          {
+            name: "泄密就在身边-最新失泄密案例",
+            path: require("../assets/video/泄密就在身边-最新失泄密案例.mp4"),
+          },
+          {
+            name: "泄密就在身边",
+            path: require("../assets/video/泄密就在身边.mp4"),
+          },
+          {
+            name: "涉密人员上网保密须知",
+            path: require("../assets/video/涉密人员上网保密须知.mp4"),
+          },
+          {
+            name: "涉密人员使用手机十二不得",
+            path: require("../assets/video/涉密人员使用手机十二不得.mp4"),
           },
         ],
         [
           {
-            name: "保密法规宣传系列之二：谨防12种保密违法违规行为4",
-            path: require("../assets/video/保密警示案例系列之一：机关门户网站的隐忧.mp4"),
+            name: "涉密会议、活动保密管理须知",
+            path: require("../assets/video/涉密会议、活动保密管理须知.mp4"),
           },
           {
-            name: "保密法规宣传系列之二：谨防12种保密违法违规行为5",
-            path: require("../assets/video/保密警示案例系列之三：兼职背后的陷阱.mp4"),
+            name: "涉密移动介质使用指南",
+            path: require("../assets/video/涉密移动介质使用指南.mp4"),
           },
           {
-            name: "保密法规宣传系列之二：谨防12种保密违法违规行为6",
-            path: require("../assets/video/保密警示案例系列之四：变味的科研合作.mp4"),
+            name: "涉密网络管理保密须知",
+            path: require("../assets/video/涉密网络管理保密须知.mp4"),
+          },
+          {
+            name: "涉密载体篇",
+            path: require("../assets/video/涉密载体篇.mp4"),
+          },
+          {
+            name: "涉密载体篇2",
+            path: require("../assets/video/涉密载体篇2.mp4"),
+          },
+          {
+            name: "领导干部、涉密人员使用互联网计算机安全保密提示",
+            path: require("../assets/video/领导干部、涉密人员使用互联网计算机安全保密提示.mp4"),
+          },
+          {
+            name: "保密法规定的十二种严重违规行为",
+            path: require("../assets/video/保密法规定的十二种严重违规行为.mp4"),
+          },
+          {
+            name: "出国出境保密须知",
+            path: require("../assets/video/出国出境保密须知.mp4"),
+          },
+          {
+            name: "哪些部门部位是保密要害部门部位",
+            path: require("../assets/video/哪些部门部位是保密要害部门部位.mp4"),
+          },
+        ],
+        [
+          {
+            name: "私人生活和个人交往保密须知",
+            path: require("../assets/video/私人生活和个人交往保密须知.mp4"),
+          },
+          {
+            name: "警钟长鸣-最新窃密泄密案例",
+            path: require("../assets/video/警钟长鸣-最新窃密泄密案例.mp4"),
+          },
+          {
+            name: "通讯办公自动化设备管理",
+            path: require("../assets/video/通讯办公自动化设备管理.mp4"),
+          },
+          {
+            name: "通讯和办公自动化涉密管理",
+            path: require("../assets/video/通讯和办公自动化涉密管理.mp4"),
           },
         ],
       ],
@@ -119,7 +199,7 @@ export default {
       console.log(val);
     },
     close() {
-      this.$refs.video.pause()
+      this.$refs.video.pause();
       this.path = null;
     },
   },
@@ -145,7 +225,7 @@ export default {
   }
   .video-page {
     clear: both;
-    padding: 20px 400px;
+    padding: 20px 470px;
   }
 }
 .videoStyle {
